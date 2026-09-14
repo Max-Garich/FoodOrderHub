7eeddc5f19ae0fd1a8172977a23abc1706048f9c
Max <max@example.com>
2026-09-14 14:49:13 +0500

    Fix stdin pipe in index check

--- deploy/check_indexes.sh
+++ deploy/check_indexes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Проверка индексов в прод-БД FoodOrderHub
-docker exec foodorderhub-db-1 psql -U foodorderhub -d foodorderhub -t <<'SQL'
+docker exec -i foodorderhub-db-1 psql -U foodorderhub -d foodorderhub -t <<'SQL'
 SELECT indexname FROM pg_indexes WHERE schemaname = 'public' ORDER BY indexname;
 SQL
